a3942e098b1117c8934b8ad205abd01cdf0148ab
Youssef el Jaddaoui <d18322@edu.rocwb.nl>
2017-03-21 15:31:29 +0100

    Website update
    Website has been made. I need to make it reponsive for mobile devices.

--- deGokkersWebsite/index.php
+++ deGokkersWebsite/index.php
@@ -1,36 +1,107 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Brostrisch</title>
-</head>
-<body>
-    /* header*/
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="style.css">
+        <link rel="shortcut icon" href="images/images-download/logojustincase.ico">
+        <title>Brostrisch</title>
+    </head>
+    <body>
 
+        <header>
+            <ul class="row-spaced">
+                <li>
+                    <a href="#a">Register</a>
+                </li>
+                <li>
+                    <a href="#a">Download</a>
+                </li>
+                <li>
+                    <a href="#a">About</a>
+                </li>
+            </ul>
+            <img src="images/images-download/websiteheaderbar.png" alt="websiteheaderbar">
+        </header>
 
+        <div class="video">
+            <h1>BROSTRISCH</h1>
+            <iframe width="854" height="480" src="https://www.youtube.com/embed/SP-lrqa5cd8" allowfullscreen></iframe>
+            <h4>Download Now!</h4>
+        </div>
 
-    /* video sectie*/
+        <div class="main">
+            <div class="download-section row-spaced">
+                <div class="image-left-download">
+                    <img src="images/images-download/imgOstrichDownload.png" alt="imgOstrichDownload">
+                </div>
+                <div class="right-content-download">
+                    <p>You must be logged in to download the program</p>
+                    <a href="test.zip">DOWNLOAD</a>
+                    <p>By downloading you agree to our terms of service</p>
+                    <h2 id="download_h1_top">Website &amp; Program</h2>
+                    <h2>Made by Youssef, Jarno & Lennard.</h2>
+                </div>
+            </div>
+            <div class="about-section">
+                <h3>ABOUT</h3>
+                <div class="bottom-content-about row-spaced">
+                    <div class="left-content-about">
+                        <img src="images/images-download/imgOstrichAbout.png" alt="imgOstrichAbout">
+                    </div>
+                    <div class="right-content-about">
+                        <p>Brostritch is een schoolproject</p>
+                        <p>gemaakt door Youssef, Jarno &amp; Lennard.</p>
+                        <p>We moesten binnen 7 weken</p>
+                        <p>het programma af hebben</p>
+                        <p>en een promotie pagina hebben.</p>
+                        <p>Bekijk onze video voor het programma uitleg.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        <div class="registerAndLogin">
+            <h3>REGISTER</h3>
+            <form action="#">
+                <div class="wrapper">
+                    <label>Voornaam:</label>
+                    <input type="text">
+                    <label>Achternaam:</label>
+                    <input type="text">
+                    <label>Gebruikersnaam:</label>
+                    <input type="text">
+                    <label>E-mailadres:</label>
+                    <input type="email">
+                    <label>Wachtwoord:</label>
+                    <input type="password" autocomplete="off">
+                </div>
+                <div class="button">
+                    <button type="submit">Verzenden</button>
+                </div>
+            </form>
+        </div>
+        <div class="registerAndLogin">
+            <h3>LOGIN</h3>
+            <form action="#">
+                <div class="wrapper">
+                    <label>Gebruikersnaam:</label>
+                    <input type="text">
+                    <label>Wachtwoord:</label>
+                    <input type="password">
+                </div>
+                <div class="button">
+                    <button type="submit">Verzenden</button>
+                </div>
+            </form>
+        </div>
 
+        <footer>
+            <a href="404.php">TOS</a>
+            <p>Contact: YJLDEV@gmail.com</p>
+            <h6>&copy; YJL Development 2017</h6>
+        </footer>
 
-    /* download sectie*/
-
-
-
-    /* about sectie*/
-
-
-
-    /* register sectie*/
-
-
-    /* login sectie*/
-
-
-    /* footer*/
-</body>
+    </body>
 </html>
