32b23b6005261f6f09aa9294e29715b94572999b
Youssef el Jaddaoui <d18322@edu.rocwb.nl>
2017-03-25 16:44:25 +0100

    Website is done
    The website is definitely done. I have cleaned the map and organised it.
    We only need to do the MySQL part.

--- deGokkersWebsite/public/index.php
+++ deGokkersWebsite/public/index.php
@@ -4,11 +4,15 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="style.css">
-        <link rel="shortcut icon" href="images/images-download/logojustincase.ico">
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="shortcut icon" href="../images/logojustincase.ico">
         <title>Brostrisch</title>
     </head>
     <body>
+
+    <!--|=========================================================================|
+        |   Header                                                                |
+        |=========================================================================|-->
 
         <header>
             <ul class="row-spaced">
@@ -22,8 +26,14 @@
                     <a href="#about">About</a>
                 </li>
             </ul>
-            <img src="images/images-download/websiteheaderbar.png" alt="websiteheaderbar">
+            <img src="../images/websiteheaderbar.png" alt="websiteheaderbar">
         </header>
+
+    <!-- End of the Header! -->
+
+    <!--|=========================================================================|
+        |   Video                                                                 |
+        |=========================================================================|-->
 
         <div class="video">
             <h1>BROSTRISCH</h1>
@@ -31,28 +41,34 @@
             <h4>Download Now!</h4>
         </div>
 
+    <!-- End of the Video! -->
+
+    <!--|=========================================================================|
+        |   Main                                                                  |
+        |=========================================================================|-->
+
         <div class="main" id="download">
             <div class="download-section row-spaced">
                 <div class="image-left-download">
-                    <img src="images/images-download/imgOstrichDownload.png" alt="imgOstrichDownload">
+                    <img src="../images/imgOstrichDownload.png" alt="imgOstrichDownload">
                 </div>
                 <div class="right-content-download">
                     <p>You must be logged in to download the program</p>
-                    <a href="test.zip">DOWNLOAD</a>
+                    <a href="../app/Application/Application.zip">DOWNLOAD</a>
                     <p>By downloading you agree to our terms of service</p>
-                    <h2>Website &amp; Program</h2>
-                    <h2>Made by Youssef, Jarno & Lennard.</h2>
+                    <h2>Website &#38; Program</h2>
+                    <h2>Made by Youssef, Jarno &#38; Lennard.</h2>
                 </div>
             </div>
             <div class="about-section" id="about">
                 <h3>ABOUT</h3>
                 <div class="bottom-content-about row-spaced">
                     <div class="left-content-about">
-                        <img src="images/images-download/imgOstrichAbout.png" alt="imgOstrichAbout">
+                        <img src="../images/imgOstrichAbout.png" alt="imgOstrichAbout">
                     </div>
                     <div class="right-content-about">
                         <p>Brostritch is een schoolproject</p>
-                        <p>gemaakt door Youssef, Jarno &amp; Lennard.</p>
+                        <p>gemaakt door Youssef, Jarno &#38; Lennard.</p>
                         <p>We moesten binnen 7 weken</p>
                         <p>het programma af hebben</p>
                         <p>en een promotie pagina hebben.</p>
@@ -61,6 +77,12 @@
                 </div>
             </div>
         </div>
+
+    <!-- End of the Main! -->
+
+    <!--|=========================================================================|
+        |   Register and login                                                    |
+        |=========================================================================|-->
 
         <div class="registerAndLogin" id="register">
             <h3>REGISTER</h3>
@@ -97,11 +119,19 @@
             </form>
         </div>
 
+    <!-- End of the Register and Login! -->
+
+    <!--|=========================================================================|
+        |   Footer                                                                |
+        |=========================================================================|-->
+
         <footer>
             <a href="404.php">TOS</a>
             <p>Contact: YJLDEV@gmail.com</p>
-            <h6>&copy; YJL Development 2017</h6>
+            <h6>&#169; YJL Development 2017</h6>
         </footer>
+
+    <!-- End of the whole website! -->
 
     </body>
 </html>
