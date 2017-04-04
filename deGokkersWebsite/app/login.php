0f3b79d1cc621b37125c862ce32a3de7f59c6337
Youssef el Jaddaoui <d18322@edu.rocwb.nl>
2017-04-04 20:10:50 +0200

    Changed the errors
    I changed the errors in de application and the website. It should work
    fine now.

--- deGokkersWebsite/app/login.php
+++ deGokkersWebsite/app/login.php
@@ -10,28 +10,43 @@ $sth = $database->prepare($sql);
 $sth->execute();
 $count = $sth->rowCount();
 
-if ($count > 0)
+if(!isset($usernameLogin) || trim($usernameLogin) == '')
 {
-    $passwordHash = hash('ripemd160', $passwordLogin);
-    $sql = "SELECT password FROM tbl_accounts WHERE password = '$passwordHash'";
-    $sth = $database->prepare($sql);
-    $sth->execute();
-    $count = $sth->rowCount();
-
-    if ($count > 0)
+    $message = " Login failed! You did not fill out the username field!";
+    header("location: ../public/index.php?message=$message");
+}
+else
+{
+    if(!isset($passwordLogin) || trim($passwordLogin) == '')
     {
-        session_start();
-        $_SESSION['user'] = $usernameLogin;
-
-        $message = "Login Succesvol!";
+        $message = "Login failed! You did not fill out the password field!";
         header("location: ../public/index.php?message=$message");
     }
     else{
-        $message = "Wrong password or username!";
-        header("location: ../public/index.php?message=$message");
+        if ($count > 0)
+        {
+            $passwordHash = hash('ripemd160', $passwordLogin);
+            $sql = "SELECT password FROM tbl_accounts WHERE password = '$passwordHash'";
+            $sth = $database->prepare($sql);
+            $sth->execute();
+            $count = $sth->rowCount();
+
+            if ($count > 0)
+            {
+                session_start();
+                $_SESSION['user'] = $usernameLogin;
+
+                $message = "Login Succesvol!";
+                header("location: ../public/index.php?message=$message");
+            }
+            else{
+                $message = "Login failed! Wrong password!";
+                header("location: ../public/index.php?message=$message");
+            }
+        }
+        else{
+            $message = "Login failed! Account doesn't exits!";
+            header("location: ../public/index.php?message=$message");
+        }
     }
-}
-else{
-    $message = "Account doesn't exits!";
-    header("location: ../public/index.php?message=$message");
 }
--- deGokkersWebsite/app/register.php
+++ deGokkersWebsite/app/register.php
@@ -14,65 +14,91 @@ $userLastName = preg_replace('/\s+/', '', $userLastName);
 $userName = preg_replace('/\s+/', '', $userName);
 $userPassword = preg_replace('/\s+/', '', $userPassword);
 
-    if (strlen($userFirstName)> 1)
+if (strlen($userFirstName)> 1)
+{
+    if (strlen($userLastName) > 1)
     {
-        if (strlen($userLastName) > 1)
+        if (strlen($userName) > 1)
         {
-            if (strlen($userName) > 1)
+            $sql = "SELECT username FROM tbl_accounts WHERE username = '$userName'";
+            $sth = $database->prepare($sql);
+            $sth->execute();
+            $count = $sth->rowCount();
+            if ($count > 0)
             {
-                    if (!filter_var($userEmail, FILTER_VALIDATE_EMAIL) == false)
+                $message = "Register failed! Username is used!";
+                header("location: ../public/index.php?message=$message");
+            }
+            else{
+
+                if (!filter_var($userEmail, FILTER_VALIDATE_EMAIL) == false)
+                {
+                    $sql = "SELECT email FROM tbl_accounts WHERE email = '$userEmail'";
+                    $sth = $database->prepare($sql);
+                    $sth->execute();
+                    $count = $sth->rowCount();
+                    if ($count > 0)
                     {
-                            if(
-                                // ik heb dit van http://stackoverflow.com/questions/5576160/checking-a-password-for-upper-lower-numbers-and-symbols
-
-                                ctype_alnum($userPassword) // numbers & digits only
-                                && strlen($userPassword)>6 // at least 7 chars
-                                && preg_match('`[A-Z]`',$userPassword) // at least one upper case
-                                && preg_match('`[a-z]`',$userPassword) // at least one lower case
-                                && preg_match('`[0-9]`',$userPassword) // at least one digit
-                            )
-                            {
-                                try {
-                                    $passwordHash = hash('ripemd160', $userPassword);
-
-                                    $sql = "INSERT INTO tbl_accounts (email, first_name, last_name, username, password) VALUES ('$userEmail','$userFirstName','$userLastName','$userName','$passwordHash')";
-                                    $database->query($sql);
-                                    $sth = $database->prepare($sql);
-                                    $sth->execute();
-                                    $count = $sth->rowCount();
-
-                                }catch (PDOException $e)
-                                {
-                                    echo "connection failed" . $e->getMessage();
-                                }
-
-                                $messageRegister = "Thanks for signing up!";
-                                header("location: ../public/index.php?message=$messageRegister");
-                            }
-                            else
-                            {
-                                $messageRegister = "Password needs to be a minimum of 7 characters with 1 digit and 1 uppercase character!";
-                                header("location: ../public/index.php?message=$messageRegister");
-                            }
+                        $message = "Register failed! Email is used!";
+                        header("location: ../public/index.php?message=$message");
                     }
-                    else {
-                        $messageRegister = "Not a valid Email!";
-                        header("location: ../public/index.php?message=$messageRegister");
+
+                    else
+                    {
+                        if(
+                            // ik heb dit van http://stackoverflow.com/questions/5576160/checking-a-password-for-upper-lower-numbers-and-symbols
+
+                            ctype_alnum($userPassword) // numbers & digits only
+                            && strlen($userPassword)>6 // at least 7 chars
+                            && preg_match('`[A-Z]`',$userPassword) // at least one upper case
+                            && preg_match('`[a-z]`',$userPassword) // at least one lower case
+                            && preg_match('`[0-9]`',$userPassword) // at least one digit
+                        )
+                        {
+                            try {
+                                $passwordHash = hash('ripemd160', $userPassword);
+
+                                $sql = "INSERT INTO tbl_accounts (email, first_name, last_name, username, password) VALUES ('$userEmail','$userFirstName','$userLastName','$userName','$passwordHash')";
+                                $database->query($sql);
+                                $sth = $database->prepare($sql);
+                                $sth->execute();
+                                $count = $sth->rowCount();
+
+                            }catch (PDOException $e)
+                            {
+                                echo "connection failed" . $e->getMessage();
+                            }
+
+                            $messageRegister = "Register completed! Thanks for signing up!";
+                            header("location: ../public/index.php?message=$messageRegister");
+                        }
+                        else
+                        {
+                            $messageRegister = "Register failed! Password needs to be a minimum of 7 characters with 1 digit and 1 uppercase character!";
+                            header("location: ../public/index.php?message=$messageRegister");
+                        }
                     }
+                }
+                else {
+                    $messageRegister = "Register failed! Not a valid Email!";
+                    header("location: ../public/index.php?message=$messageRegister");
+                }
             }
-            else
-            {
-                $messageRegister = "Username must be at least 2 characters!";
-                header("location: ../public/index.php?message=$messageRegister");
-            }
+
         }
         else
         {
-            $messageRegister = "Last name must be at least 2 characters!";
+            $messageRegister = "Register failed! Username must be at least 2 characters!";
             header("location: ../public/index.php?message=$messageRegister");
         }
     }
-    else{
-        $messageRegister = "First name must be at least 2 characters!";
+    else
+    {
+        $messageRegister = "Register failed! Last name must be at least 2 characters!";
         header("location: ../public/index.php?message=$messageRegister");
     }
+}
+else{
+    $messageRegister = "Register failed! First name must be at least 2 characters!";
+    header("location: ../public/index.php?message=$messageRegister");
+}
